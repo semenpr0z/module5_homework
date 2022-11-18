c510b75ec602e8d067d45a8b5e986a4ec09c7c36
Semen <justpr0z.com.ru@gmail.com>
2022-11-18 20:48:45 +0300

    added 6 exercise

---
+++ exercise6.js
@@ -0,0 +1,11 @@
+const arr = [1, 1, 1, 1, 1];
+let result = false;
+for (let i = 0; i<arr.length; i++){
+  if (arr[i] === arr[0]){
+    result = true;
+  }
+  else {
+    result = false;
+  }
+};
+console.log(result)
